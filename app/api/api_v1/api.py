c4092341b36fe5996cd64ae069eeb41c7a7110b6
BhagyaChengta <133753313+BhagyaChengta@users.noreply.github.com>
2023-08-04 14:17:15 +0530

    Update api.py

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -5,7 +5,7 @@ from app.api.api_v1.endpoints.session import endpoints as session_endpoints
 from app.api.api_v1.endpoints.preprocessing import endpoints as preprocessing_endpoints
 from app.api.api_v1.endpoints.modelling import endpoints as modelling_endpoints
 from app.api.api_v1.endpoints.status import endpoints as status_endpoints
-from app.api.api_v1.endpoints.deploy import endpoints as deploy_endpoints
+# from app.api.api_v1.endpoints.deploy import endpoints as deploy_endpoints
 
 
 
@@ -15,6 +15,6 @@ api_router.include_router(db_endpoints.router, prefix="/db", tags=["Database API
 api_router.include_router(preprocessing_endpoints.router, prefix="/preprocess", tags=["Preprocessing APIs"])
 api_router.include_router(modelling_endpoints.router, prefix="/model", tags=["Modelling APIs"])
 api_router.include_router(status_endpoints.router, prefix="/Status API", tags=["Status API"])
-api_router.include_router(deploy_endpoints.router, prefix="/Deployment API", tags=["DEPLOYMENT API"])
+# api_router.include_router(deploy_endpoints.router, prefix="/Deployment API", tags=["DEPLOYMENT API"])
 
 
