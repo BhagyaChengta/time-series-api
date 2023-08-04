67d4ba53a1a7af52321e4180423f416dad7e4040
BhagyaChengta <133753313+BhagyaChengta@users.noreply.github.com>
2023-08-04 14:11:20 +0530

    Update endpoints.py

--- app/api/api_v1/endpoints/deploy/endpoints.py
+++ app/api/api_v1/endpoints/deploy/endpoints.py
@@ -1,21 +1,21 @@
-from app.controller.deployment.deploy import DeploymentModel
-from app.models.models import deployModelDetails
-from fastapi import APIRouter
-from app.core.config import settings as aws_settings
-from app.models.models import General_Response
+#  from app.controller.deployment.deploy import DeploymentModel
+# from app.models.models import deployModelDetails
+# from fastapi import APIRouter
+# from app.core.config import settings as aws_settings
+# from app.models.models import General_Response
 
 
-router = APIRouter()
+# router = APIRouter()
 
 
-import subprocess
+# import subprocess
 
-@router.post("/deploy-model", status_code=200)
-def deploy_endpoint_route(*, data: deployModelDetails):
-    obj = DeploymentModel(data)
+# @router.post("/deploy-model", status_code=200)
+# def deploy_endpoint_route(*, data: deployModelDetails):
+#     obj = DeploymentModel(data)
 
-    # Deploy the model and return the endpoint
-    endpoint_res = obj.deploy_model()
+#     # Deploy the model and return the endpoint
+#     endpoint_res = obj.deploy_model()
     
     
-    return endpoint_res
+#     return endpoint_res
